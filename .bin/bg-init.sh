f1b77c606628c5bf71531d765cf93c91f1a3f5ca
juvo <juvor@outlook.com>
2026-04-13 12:39:39 +1000

    misc: output fixing, egpu config

--- .bin/bg-init.sh
+++ .bin/bg-init.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-pkill swww-daemon
-swww-daemon --format xrgb &
+pkill awww-daemon
+awww-daemon --format xrgb &
 
-while ! swww query >/dev/null 2>&1; do
+while ! awww query >/dev/null 2>&1; do
     sleep 0.2
 done
 
-swww img ~/.config/sway/assets/3.jpg
+awww img ~/.config/sway/assets/3.jpg
